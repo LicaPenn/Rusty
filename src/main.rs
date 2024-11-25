be748555f502b744d933f28377d1d0e66ac0cbd6
Lica Penninger <ap1414@student.aru.ac.uk>
2024-11-25 17:04:09 +0000

    path changes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
 use bio::io::fasta;
-
+use std::env;
 fn main() -> () {
-
-
-
-    let sh = match fasta::Reader::from_file("./data/sequence.fasta") {
+    let mut arguments= Vec::new();
+    for argument in env::args() {
+        arguments.push(argument); 
+    }
+    let path_to_file= &arguments[1];
+    let sh = match fasta::Reader::from_file(path_to_file) {
         Ok(val) => val,
         Err(_e) => panic!("could not read fasta file"),
     };
     let mut gcount = 0;
-    let mut ccount= 0;
-    let mut allcount=0; 
+    let mut ccount = 0;
+    let mut allcount = 0;
     for result in sh.records() {
         let record = result.unwrap();
         let sequence = record.seq();
         for letter in sequence {
-            if letter == &71u8 {gcount +=1} 
-            if letter == &67u8 {ccount +=1} 
-            allcount +=1;
-
+            if letter == &71u8 {
+                gcount += 1
+            }
+            if letter == &67u8 {
+                ccount += 1
+            }
+            allcount += 1;
         }
-   let gcprop= (gcount+ccount) as f64 / allcount as f64;
-        println!("Number of Gs: {}, number of Cs: {}, total number:{}, proportion of gc:{}", gcount, ccount, allcount,gcprop)
+        let gcprop = (gcount + ccount) as f64 / allcount as f64;
+        println!(
+            "Number of Gs: {}, number of Cs: {}, total number:{}, proportion of gc:{}",
+            gcount, ccount, allcount, gcprop
+        )
     }
-
- 
 }
-
