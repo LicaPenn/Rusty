36634fda455e0e0fdf519747b296c064b0ffbcc8
Lica Penninger <ap1414@student.aru.ac.uk>
2024-12-02 16:59:37 +0000

    better error system, added command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
+use anyhow::{Context, Result};
 use bio::io::fasta;
-use std::env;
-fn main() -> () {
-    let mut arguments= Vec::new();
-    for argument in env::args() {
-        arguments.push(argument); 
-    }
-    let path_to_file= &arguments[1];
-    let sh = match fasta::Reader::from_file(path_to_file) {
-        Ok(val) => val,
-        Err(_e) => panic!("could not read fasta file"),
-    };
+use clap::{arg, command, value_parser, ArgAction, Command};
+use std::path::PathBuf;
+
+fn main() -> Result<()> {
+    let matches = command!()
+    .about("Counts G and C of your fasta file")
+        .arg(
+            arg!(
+                 <FILE> "Path to Fasta file"
+            )
+           
+            .required(true)
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+   let path_to_file = matches.get_one::<PathBuf>("FILE").unwrap();
+
+    let sh =
+        fasta::Reader::from_file(path_to_file).context("Did you spell the path name correctly?")?;
     let mut gcount = 0i64;
     let mut ccount = 0i64;
     let mut allcount = 0i64;
     for result in sh.records() {
-        let record = result.unwrap();
+        let record = result.context("Could not read record.")?;
         let sequence = record.seq();
         for letter in sequence {
             if letter == &71u8 {
@@ -29,6 +39,7 @@ fn main() -> () {
         println!(
             "Number of Gs\tNumber of Cs\tTotal number\tProportion of GC\n{gcount}\t{ccount}\t{allcount}\t{gcprop}"
         )
-
     }
+
+    Ok(())
 }
